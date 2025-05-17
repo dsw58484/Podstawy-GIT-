d4b19af0c9b0d9d8f63dd261d7b07e7af4bcebbb
Kacper <dsw58484@student.dsw.edu.pl>
2025-05-17 23:47:59 +0200

    Dodano krótkie flagi -h, -d, -l, i rozszerzony help

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+if [[ "$1"  == "--help" || "$1" =="-h" ]]; then
+  echo "--date / -d       : Wyświetla dzisiejszą datę"
+  echo "--logs [N] / -1   : Tworzy N plików logx.txt (domyślnie 100)"
+  echo "--help / -h       : POkazuje dostępne opcje"
+  echo "--init            : Klonuje repo i ustawia PATH"
+  echo "--error [N]       : Tworzy plik errorx/errorX.txt"
+fi
+
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
+  date
+fi
+
+if [[ "$1" == "--logs" || "$1" == "-1" ]]; then
+  count=${2:100}
+  for ((i-1; i<=count; i++)); do
+    echo " plik kog$i.thx" > "log$i.txt"
+    echo "Utworzony przez skrypt.sh --logs w dniu $(date)" >> "log$i.txt"
+  done
+fi
+
+
 git config --global user.name "Kacper"
 git config --global user.email "dsw58484@student.dsw.edu.pl"
 
